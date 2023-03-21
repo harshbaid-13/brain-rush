79e1e25e63076285d42aa6da827b8f8da9fcfbc1
anamika-20 <anamikatiwary20@gmail.com>
2023-03-21 20:30:10 +0530

    Navbar

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 import Details from "@/components/Details";
 import Footer from "@/components/Footer";
 import Hero from "@/components/Hero";
+import Navbar from "@/components/Navbar";
 export default function Page() {
   return (
     <>
+      <Navbar />
       <Hero />
       <Details />
       <Footer />
