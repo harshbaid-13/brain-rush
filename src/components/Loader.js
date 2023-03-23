66091c3b610d77c417437aeb138bf3ebf467661f
am-here <harshbaid.ind@gmail.com>
2023-03-23 14:15:55 +0530

    nothing much

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,6 +1,5 @@
 import Image from "next/image";
 import React from "react";
-import "./loader.css";
 function Loader(props) {
   return (
     <div className="fixed flex inset-0 z-40 w-full h-full justify-center items-center">
