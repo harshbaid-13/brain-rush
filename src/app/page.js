5da80465b5bcd1f7fd7e01ef69805ad59db2b43a
am-here <harshbaid.ind@gmail.com>
2023-03-23 16:28:30 +0530

    loader in mobile -- solved

--- src/app/page.js
+++ src/app/page.js
@@ -15,7 +15,7 @@ export default function Page() {
   useEffect(() => {
     setTimeout(() => {
       setLoading(false);
-    }, 4500);
+    }, 4000);
   }, []);
   return (
     <>
--- src/components/Loader.js
+++ src/components/Loader.js
@@ -2,8 +2,8 @@ import Image from "next/image";
 import React from "react";
 function Loader(props) {
   return (
-    <div className="fixed flex inset-0 z-50 w-full h-full justify-center items-center">
-      <div className="">
+    <div className="fixed flex justify-center items-center inset-0 z-50 w-full h-full bg-background">
+      <div>
         <Image src={props.gif} alt="loader" draggable={false} />
       </div>
     </div>
